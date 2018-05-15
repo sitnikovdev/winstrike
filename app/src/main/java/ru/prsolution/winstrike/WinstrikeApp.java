eb6f6d81e7f783c0c5068b7be2975e7109bde552
oleg <o.sitnikov@pr-solution.ru>
2018-05-15 17:49:00 +0300

    Add  Leak Canary

--- app/src/main/java/ru/prsolution/winstrike/WinstrikeApp.java
+++ app/src/main/java/ru/prsolution/winstrike/WinstrikeApp.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.os.Environment;
 import android.util.DisplayMetrics;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -30,6 +32,12 @@ public class WinstrikeApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
         INSTANCE = this;
     }
 
