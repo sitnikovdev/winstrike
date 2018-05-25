55b24e62206342144b8bbdb6f6298f35b9cf5f7f
oleg <o.sitnikov@pr-solution.ru>
2018-05-25 19:36:07 +0300

    Fix size: remove unused images from resources.

--- app/src/main/java/ru/prsolution/winstrike/mvp/models/SeatType.kt
+++ app/src/main/java/ru/prsolution/winstrike/mvp/models/SeatType.kt
@@ -1,43 +1,10 @@
 package ru.prsolution.winstrike.mvp.models
 
-//
-//  SeatType.swift
-//  winstrike
-//
-//  Created by PRS on 28/02/2018.
-//  Copyright © 2018 PR_Solution. All rights reserved.
-//
 
-
-import ru.prsolution.winstrike.R
 import java.util.*
 
 enum class SeatType private constructor(val type: String) {
     FREE("free"), HIDDEN("hidden"), SELF_BOOKING("self_booking"), BOOKING("booking"), VIP("vip");
-
-    val image: Int
-        get() {
-            if (this == FREE) {
-                return R.drawable.seat_grey
-            }
-
-            if (this == SELF_BOOKING) {
-                return R.drawable.seat_blue
-            }
-
-            if (this == BOOKING) {
-                return R.drawable.seat_red
-            }
-
-            if (this == VIP) {
-                return R.drawable.seat_yellow
-            }
-
-            return if (this == HIDDEN) {
-                R.drawable.seat_darkgrey
-            } else R.drawable.seat_grey
-
-        }
 
     companion object {
 
@@ -56,80 +23,6 @@ enum class SeatType private constructor(val type: String) {
         operator fun get(status: String): SeatType? {
             return lookup[status]
         }
-
-        // TODO Get image resources for seatApi status
-       fun getImage(status: SeatType): Int? {
-            when (status) {
-                SeatType.FREE -> return R.drawable.ic_seat_gray
-                SeatType.BOOKING -> return R.drawable.ic_seat_red
-                SeatType.SELF_BOOKING -> return R.drawable.ic_seat_blue
-                SeatType.HIDDEN -> return R.drawable.ic_seat_darkgray
-                SeatType.VIP -> return R.drawable.ic_seat_yellow
-                else -> {
-                    return R.drawable.ic_seat_gray
-                }
-            }
-
-        }
     }
 }
 
-/* enum SeatType: String {
-    case available = "free"
-    case vip
-    case bookedYou = "self_booking"
-    case picked
-    case booked = "booking"
-    case hidden = "hidden"
-
-    func getImage() -> UIImage {
-        switch self {
-        case .available:
-            return Asset.ChooseSeat.seatGrey.image
-        case .booked:
-            return Asset.ChooseSeat.seatRed.image
-        case .picked:
-            return Asset.ChooseSeat.seatWhite.image
-        case .bookedYou:
-            return Asset.ChooseSeat.seatBlue.image
-        case .vip:
-            return Asset.ChooseSeat.seatYellow.image
-        case .hidden:
-            return Asset.ChooseSeat.seatDarkGrey.image
-        }
-    }
-
-    func getSrc() -> String {
-        switch self {
-        case .available:
-            return "seatGrey.png"
-        case .booked:
-            return "seatRed.png"
-        case .picked:
-            return "seatWhite.png"
-        case .bookedYou:
-            return "seatBlue.png"
-        case .vip:
-            return "seatYellow.png"
-        case .hidden:
-            return "seatDarkGrey.png"
-        }
-    }
-
-    func getDesc() -> String {
-        switch self {
-        case .available:
-            return L10n.seatPickerSeatTypeAvailable
-        case .booked:
-            return L10n.seatPickerSeatTypeBooked
-        case .bookedYou:
-            return L10n.seatPickerSeatTypeBookedYou
-        case .picked:
-            return L10n.seatPickerSeatTypePicked
-        case .vip:
-            return L10n.seatPickerSeatTypeVip
-        case .hidden:
-            return L10n.seatPickerSeatTypeHidden
-        }
-}
-    }*/
