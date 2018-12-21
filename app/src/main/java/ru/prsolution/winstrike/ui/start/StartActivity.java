9be07f24f5bafca3220761a8061741d219a063de
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2018-12-21 15:16:29 +0300

    remove commented code from StartActivity

--- app/src/main/java/ru/prsolution/winstrike/ui/start/StartActivity.java
+++ app/src/main/java/ru/prsolution/winstrike/ui/start/StartActivity.java
@@ -19,30 +19,6 @@ import timber.log.Timber;
 public class StartActivity extends AppCompatActivity {
 
 
-/*    private class CrashlyticsTree extends Timber.Tree {
-        private static final String CRASHLYTICS_KEY_PRIORITY = "priority";
-        private static final String CRASHLYTICS_KEY_TAG = "tag";
-        private static final String CRASHLYTICS_KEY_MESSAGE = "message";
-
-        @Override
-        protected void log(int priority, String tag, String message, Throwable t) {
-
-            if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
-                return;
-            }
-
-            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
-            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
-            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
-
-            if (t == null) {
-                Crashlytics.logException(new Exception(message));
-            } else {
-                Crashlytics.logException(t);
-            }
-        }
-    }*/
-
     public class NotLoggingTree extends Timber.Tree {
         @Override
         protected void log(final int priority, final String tag, final String message, final Throwable throwable) {
