45f0086cddcff80f51d745f757fa9b657b86e7dc
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-01-15 17:46:42 +0300

    fix gradle

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,29 @@
+object ApplicationId {
+    val id = ""
+}
+
+object Modules {
+    val app = ":app"
+}
+
+object Release {
+    val versionCode = 1
+    val versionName = ""
+}
+
+object Versions {
+    val gradle = "3.4.0-alpha10"
+    val compileSdk = "28"
+    val minSdk = "21"
+    val targetSdk = "28"
+    val googleServicies = "4.2.0"
+    val firebase = "16.0.4"
+    val fabric = "1.27.0"
+    val appCompat = "1.0.2"
+    val design = "1.0.0"
+    val cardView = "1.0.0"
+    val recyclerView = "1.0.0"
+
+    val kotlin = "1.3.11"
+    val dataBinding = "3.2.0-alpha10"
+}
