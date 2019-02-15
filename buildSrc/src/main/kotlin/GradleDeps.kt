49d4639c44eb87c176b12c1f7b10487ae421f1fc
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-02-15 17:20:53 +0300

    v0.50.1

--- buildSrc/src/main/kotlin/GradleDeps.kt
+++ buildSrc/src/main/kotlin/GradleDeps.kt
@@ -2,7 +2,7 @@ object AppVersion {
 	// App version information
 	const val majorAppVersion = 0
 	const val minorAppVersion = 50
-	const val patchAppVersion = 0
+	const val patchAppVersion = 1
 	const val buildAppVersion = 0
 }
 
